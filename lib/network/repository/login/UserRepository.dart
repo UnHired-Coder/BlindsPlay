e2cc2e9ea9aae9ff530228c2f4524d176478ac74
PRATYUSH TIWARI <42827589+UnHired-Coder@users.noreply.github.com>
2024-10-26 10:10:23 +0530

    Assigns Random Guest User Name

--- lib/network/repository/login/UserRepository.dart
+++ lib/network/repository/login/UserRepository.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'UserService.dart';
@@ -9,11 +11,28 @@ class UserRepository {
       : _userService = userService;
 
   Future<void> loginUser(User user, String authType) async {
+    final String randomName = user.displayName ?? generateRandomGuestName();
+
     await _userService.loginUser(
       userId: user.uid,
-      name: user.displayName ?? 'Anonymous',
+      name: randomName,
       email: user.email ?? 'Anonymous@email.com',
       authType: authType,
     );
+  }
+
+// Helper function to generate a random guest name
+  String generateRandomGuestName() {
+    const length = 4;
+    const chars =
+        'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    final random = Random();
+
+    // Generate a random string of length `length`
+    final randomString =
+        List.generate(length, (index) => chars[random.nextInt(chars.length)])
+            .join();
+
+    return 'Guest-$randomString';
   }
 }
