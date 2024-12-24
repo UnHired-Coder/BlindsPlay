e28d0dc7624c9f8bd0385eac0cf8b0fe506072e5
PRATYUSH TIWARI <42827589+UnHired-Coder@users.noreply.github.com>
2024-12-24 16:31:17 +0530

    Updates: Prod URLS

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -9,17 +9,25 @@ class AppConstants {
 
   static const String fontFamily1 = "FiraSans";
 
-  //staging
+  //staging-emulator
   // static const String BASE_URL = "http://10.0.2.2:8080";
   // static const String BASE_WS_URL = "ws://10.0.2.2:8080";
 
-  //staging
+  //staging-web
   // static const String BASE_URL = "http://0.0.0.0:8080";
   // static const String BASE_WS_URL = "ws://0.0.0.0:8080";
 
-  //prod
+  //prod-replit
   static const String BASE_URL =
       "https://0a587489-1ca7-4ac8-933d-4e058aeeb8f3-00-1ycyvf91o39v8.pike.replit.dev";
   static const String BASE_WS_URL =
       "wss://0a587489-1ca7-4ac8-933d-4e058aeeb8f3-00-1ycyvf91o39v8.pike.replit.dev";
+
+  //prod-vercel
+  // static const String BASE_URL = "https://tictacmemo-server.vercel.app";
+  // static const String BASE_WS_URL = "wss://tictacmemo-server.vercel.app";
+
+  //prod-onrender
+  // static const String BASE_URL = "https://tictacmemo-server.onrender.com";
+  // static const String BASE_WS_URL = "wss://tictacmemo-server.onrender.com";
 }
